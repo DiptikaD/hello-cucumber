5f499df135d33fe6569d73a78e650625d73081af
DiptikaD <diptikadevi@gmail.com>
2025-02-25 11:42:13 -0500

    adjusted skeletal methods with logic

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -4,22 +4,30 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class IsItFriday{
     static String isItFriday(String today){
-        return null;
+        return "Nope";
     }
 }
 
 public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
     @Given("today is Sunday")
     public void todayIsSunday() {
+        today="Sunday";
     }
 
     @When("I ask whether it's Friday yet")
     public void iAskWhetherItSFridayYet() {
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("I should be told {string}")
-    public void iShouldBeTold(String arg0) {
+    public void iShouldBeTold(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
     }
 }
